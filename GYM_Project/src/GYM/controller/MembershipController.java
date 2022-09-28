13805dd85602d78bb79df798370fa04676c240b7
504 <504@DESKTOP-JGPOC4B>
2022-09-28 17:44:10 +0900

    회원권관리 마무리

--- GYM_Project/src/GYM/controller/MembershipController.java
+++ GYM_Project/src/GYM/controller/MembershipController.java
@@ -1,5 +1,29 @@
 package GYM.controller;
 
-public class MembershipController {
+import java.util.ArrayList;
 
+import GYM.model.Dao.MembershipDAO;
+import GYM.model.Dto.MembershipDTO;
+
+public class MembershipController {
+	
+	public boolean create(String mem_name,String mem_start,String mem_end,String mem_comment) {
+		MembershipDTO mem_dto = new MembershipDTO(0,mem_name,mem_start,mem_end,mem_comment);
+		return MembershipDAO.getInstance().create(mem_dto);
+	}
+	
+	public ArrayList<MembershipDTO> read(){
+		return MembershipDAO.getInstance().read();
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
