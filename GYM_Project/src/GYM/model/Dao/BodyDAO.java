37c393688e3264c63a8963a779be9510e2a52c53
504 <504@DESKTOP-CRHLONI>
2022-09-30 16:21:15 +0900

    타임

--- GYM_Project/src/GYM/model/Dao/BodyDAO.java
+++ GYM_Project/src/GYM/model/Dao/BodyDAO.java
@@ -1,12 +1,14 @@
 package GYM.model.Dao;
 
 import java.sql.Connection;
+
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import GYM.model.Dto.BodyDTO;
+
 
 public class BodyDAO {
 	// 필드
@@ -29,17 +31,23 @@ public class BodyDAO {
 		}
 	}
 	
+	// 성생
 	public static BodyDAO getInstance() { return dao; }
 
-	/*public boolean profile (BodyDTO bodyDTO) {
-		
-		try {
-			
-			
-		}catch (Exception e) { }
-	}
+	boolean create(BodyDAO dto) {
+			String sql = "insert into expense values( null , ? , ? , ? );";
+			try {
+				ps = con.prepareStatement(sql);
+				ps.setString( 1 , dto.toString() );
+				ps.setInt( 2 , dto.hashCode() );
+				ps.setString( 3 , dto.toString() );
+				ps.executeUpdate(); return true;
+			}catch (Exception e) { System.out.println("DB오류: "+e);}
+			return false;
+			}
 	
-	// 1 */
+	// 1 
+	 
 	
 	
 
